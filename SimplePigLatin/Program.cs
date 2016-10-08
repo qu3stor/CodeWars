04c6497d2d2f52b2f565a3871896b94e62c0217b
mix <mix>
2016-10-09 00:14:55 +0300

    added solution SimplePigLatin

---
+++ SimplePigLatin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PigIt("Pig latin is cool"));
+            Console.ReadKey();
+        }
+
+        public static string PigIt(string str)
+        {
+
+            string[] array = str.Split(' ');
+            var pigged = array.Select(word => (word + word.First()).Substring(1)+"ay");
+            string output = String.Join(" ", pigged);
+
+
+            return output;
+
+        }
+    }
+}
