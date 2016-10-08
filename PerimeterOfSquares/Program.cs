269af7db330917799a6010b5591f775a55650a1e
mix <mix>
2016-10-08 23:28:26 +0300

    added PerimeterOfSquares solution

--- PerimeterOfSquares/Program.cs
+++ PerimeterOfSquares/Program.cs
@@ -10,6 +10,35 @@ namespace PerimeterOfSquares
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Fibo(7));
+            Console.ReadKey();
         }
+
+
+        public static long Fibo(long n)
+        {
+            List<long> fiboNumbers = new List<long>();
+            long a = 1;
+            long b = 1;
+            fiboNumbers.Add(a*4);
+            fiboNumbers.Add(b*4);
+
+            for (int i = 3; i <= n + 1; i++)
+            {
+                long c = a + b;
+                a = b;
+                b = c;
+                fiboNumbers.Add(c*4);
+            }
+
+            long total = 0;
+            foreach (long number in fiboNumbers)
+            {
+                total += number;
+            }
+
+            return total;
+        }
+        
     }
 }
