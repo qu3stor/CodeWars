9bb7b41be3c6ea831f513aefb4d6aa5b1f3daab0
Q <Q-PC>
2016-10-10 16:44:21 +0300

    added solution  Minimum unfairness of a list

---
+++ MinimumUnfairnessOfList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumUnfairnessOfList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinUnfairness(new int[]  { 30, 100, 1000, 150, 60, 250, 10, 120, 20 }, 0));
+            Console.ReadKey();
+        }
+
+        public static int MinUnfairness(int[] arr, int k)
+        {
+            if (arr.Length != 0 && k != 0)
+            {
+                Array.Sort(arr);
+                int minDelta = Int32.MaxValue;
+               // int iteration = 0;
+
+                for (int i = 0; i <= arr.Length - k; i++)
+                {
+                    int delta = arr[i + k - 1] - arr[i];
+                    if (delta < minDelta)
+                    {
+                        minDelta = delta;
+                        //iteration = i;
+                    }
+
+                }
+                return minDelta;
+            }
+            else
+                return 0;
+        }
+    }
+}
