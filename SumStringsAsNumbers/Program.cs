76df8e14b24d0a6a2541ea248ed18ee4f1c674de
Q <Q-PC>
2016-10-11 16:13:02 +0300

    added solution SumStringsAsNumbers

---
+++ SumStringsAsNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace SumStringsAsNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumStrings("", "81055900096023504197206408605"));
+            Console.ReadKey();
+
+        }
+
+        public static string sumStrings(string a, string b)
+        {
+            BigInteger first = new BigInteger();
+            BigInteger second = new BigInteger();
+
+            if (a != String.Empty && b != String.Empty)
+            {
+                first = BigInteger.Parse(a);
+                second = BigInteger.Parse(b);
+            }
+
+            else if (a == String.Empty)
+            {
+                second = BigInteger.Parse(b);
+            }
+            else if (b == String.Empty)
+            {
+                first = BigInteger.Parse(a);
+            }
+
+            return (first + second).ToString();
+        }
+    }
+}
