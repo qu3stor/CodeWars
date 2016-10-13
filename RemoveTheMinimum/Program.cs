d7bf4b84828c3d2df8300ba86a983e85f070c0ce
Q <Q-PC>
2016-10-13 15:24:41 +0300

    added solution RemoveTheMinimum

---
+++ RemoveTheMinimum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveTheMinimum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            List<int> listNew = RemoveSmallest(list);
+            
+            Console.ReadKey();
+        }
+
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            // Good Luck!
+            //numbers.IndexOf(numbers.Min());
+            if (numbers.Count > 0)
+            {
+                numbers.RemoveAt(numbers.IndexOf(numbers.Min()));
+            }
+            
+            return numbers;
+        }
+    }
+}
