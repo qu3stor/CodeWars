1ca8a945a3df44c5498c5d3f32a54ef8ca4a3937
mix <mix>
2016-10-15 22:15:08 +0300

    added GatherGenerics kata

---
+++ GatherGenerics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatherGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new List<IParser<BaseValue>>();
+            foo.Add(new ParserTypeA());
+            foo.Add(new ParserTypeB());
+        }
+    }
+
+    public class BaseValue
+    {
+        // some content
+    }
+
+    public class ValueTypeA : BaseValue
+    {
+        // some content
+    }
+
+    public class ValueTypeB : BaseValue
+    {
+        // some content
+    }
+
+    public class ParserTypeA : ValueTypeA
+    {
+        public ValueTypeA Parse(string value) { return new ValueTypeA(); }
+    }
+
+    public class ParserTypeB : ValueTypeB
+    {
+        public ValueTypeB Parse(string value) { return new ValueTypeB(); }
+    }
+
+    interface IParser<T>
+    {
+        BaseValue Parse(string value);
+    }
+}
