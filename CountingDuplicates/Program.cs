ebb19baf47b6a4c8950519a11206afe763ab533b
mix <mix>
2016-10-16 21:00:10 +0300

    added CountingDuplicates solution added NationalBathSafetyMonth solution

---
+++ CountingDuplicates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string test = "abc";
+            DuplicateCount(test);
+
+
+
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+        public static int DuplicateCount(string str)
+        {
+            var result = str.ToLower().GroupBy(c => c).Where(c => c.Count() > 1);
+            return result.Count();
+        }
+
+
+    }
+}
---
+++ NationalBathSafetyMonth/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalBathSafetyMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.WriteLine(Bath("VqlVOkCguPtxaof", "|~~~~|"));
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+        public static bool Bath(string s, string x)
+        {
+            // Happy Coding
+            if (s.Length <= x.Length - 2)
+            {
+                return false;
+            }
+            else
+                return true;
+
+        }
+    }
+}
