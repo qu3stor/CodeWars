d30de7ad9c93b2ec6baaef1a02b1982022ef953b
mix <mix>
2016-10-28 01:25:26 +0300

    added CompleteSeries solution

---
+++ CompleteSeries/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] test1 = { 2, 1, 2 };
+            CompleteSeries(test1);
+
+            Console.WriteLine(CompleteSeries(test1));
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+
+        public static int[] CompleteSeries(int[] arr)
+        {
+            bool unique = arr.Distinct().Count() == arr.Length;
+
+            if( unique == false)
+            {
+                return new int[] { 0 };
+            }
+            else
+            {
+                Array.Sort(arr);
+                List<int> list = new List<int>();
+                list.AddRange(Enumerable.Range(0, arr[arr.Length - 1] + 1));
+                int[] arr1 = list.ToArray();
+                return arr1;
+            }
+            
+
+            
+        }
+    }
+}
--- DeadAnts/Program.cs
+++ DeadAnts/Program.cs
@@ -11,13 +11,35 @@ namespace DeadAnts
     {
         static void Main(string[] args)
         {
-            //..anat.aa.a.t.tantnt.anntn.tn.an.t......at.n..atnt.an --> 10
-            //t.naat.a..tn.an...att.n.n.an..anttan..n.n.tant.a.t.at --> 9
+            string ants = null; // --> 10
+            //string ants = "t.naat.a..tn.an...att.n.n.an..anttan..n.n.tant.a.t.at"; // --> 9
             //string ants = "a.an.atat.at.a..aannt.t.nn.tn.n..ntt....tt..nn.t.aana"; //-->11
-            string ants = "ant anantt aantnt"; 
-            int antsAlive = Regex.Matches(ants, "ant").Count;
+            //string ants = "ant anantt aantnt"; 
+            Console.WriteLine(DeadAntCount(ants));
+            
 
             Console.ReadKey();
+        }
+
+        public static int DeadAntCount(string ants)
+        {
+            if (ants==null)
+            {
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(ants);
+                int antsAlive = Regex.Matches(ants, "ant").Count;
+                int countA = ants.Count(x => x == 'a');
+                int countN = ants.Count(x => x == 'n');
+                int countT = ants.Count(x => x == 't');
+                int max = Math.Max(countA, Math.Max(countN, countT));
+                int deadAnts = max - antsAlive;
+
+                return deadAnts;
+
+            }
         }
     }
 }
