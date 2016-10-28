f9870f5c110843205fa6fcaab2ebf40ea84f1f61
Q <Q-PC>
2016-10-28 15:53:57 +0300

    ReversePolishNotationSolver solution added

---
+++ ReversePolishNotationSolver/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversePolishNotationSolver
+{
+    class Program
+    {
+        //rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#C.23
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SolvePostfix("73 12 47 + 45 48 - 38 75 37 * + - + -"));
+            Console.ReadKey();
+        }
+
+        public static double SolvePostfix(string expression)
+        {
+            string[] arr = expression.Split(' ');
+
+            Stack<double> stack = new Stack<double>();
+            double number = 0;
+
+            foreach (var item in arr)
+            {
+                if (double.TryParse(item, out number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    switch (item)
+                    {
+                        case "^":
+                            {
+                                number = stack.Pop();
+                                stack.Push(Math.Pow(stack.Pop(), number));
+                                break;
+                            }
+                        case "*":
+                            {
+                                stack.Push(stack.Pop() * stack.Pop());
+                                break;
+                            }
+                        case "/":
+                            {
+                                number = stack.Pop();
+                                stack.Push(stack.Pop() / number);
+                                break;
+                            }
+                        case "+":
+                            {
+                                stack.Push(stack.Pop() + stack.Pop());
+                                break;
+                            }
+                        case "-":
+                            {
+                                number = stack.Pop();
+                                stack.Push(stack.Pop() - number);
+                                break;
+                            }
+                    }
+                }
+            }
+
+
+            return stack.Pop();
+        }
+    }
+}
---
+++ SimpleFractionToMixedNumberConverter/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFractionToMixedNumberConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /// Console.WriteLine(GetDelimiter(3, 7));
+            //Console.WriteLine(2 / 4);
+            Console.WriteLine(MixedFraction("0/-4"));
+            Console.ReadKey();
+        }
+
+        public static string MixedFraction(string s)
+        {
+            int a = Int32.Parse(s.Split('/')[0]);
+            int denominator = Int32.Parse(s.Split('/')[1]);
+
+            if (a == 0 && denominator !=0)
+            {
+                return "0";
+            }
+
+            if (a % denominator == 0)
+            {
+                return (a / denominator).ToString();
+            }
+
+            int intPart = a / denominator;
+            int numerator = Math.Abs(a % denominator);
+            long gcd = GetDelimiter(numerator, Math.Abs(denominator));
+            string result = String.Empty;
+
+            if (gcd == 1)
+            {
+                if (intPart == 0)
+                {
+                    result = String.Format("{0}/{1}", numerator, Math.Abs(denominator));
+                }
+                else
+                {
+                    result = String.Format("{0} {1}/{2}", intPart, numerator, Math.Abs(denominator));
+                }
+            }
+            else
+            {
+                if (intPart == 0)
+                {
+                    result = String.Format("{0}/{1}", numerator / gcd, Math.Abs(denominator) / gcd);
+                }
+                else
+                {
+                    result = String.Format("{0} {1}/{2}", intPart, numerator / gcd, Math.Abs(denominator) / gcd);
+                }
+                
+            }
+            return result;
+        }
+
+
+        public static long GetDelimiter(long a, long b)
+        {
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    long tmp = a;
+                    a = b;
+                    b = tmp;
+                }
+                b = b - a;
+            }
+            return a;
+        }
+    }
+}
